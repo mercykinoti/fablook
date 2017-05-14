426b8b371ceb59ead344c571f728ad609275c5d4
mercykinoti <mkinoti69@gmail.com>
2017-05-14 17:59:11 +0000

    template

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,6 +2,9 @@ class ChoicesController < ApplicationController
   def index
     @choices = Choice.all
   end
+  def show
+    @choice = Choice.find(params[:id])
+  end
   def new
     @choice = Choice.new
   end
