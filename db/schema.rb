f153cd14449909ed0c5b8648aaef130d0d27621e
mercykinoti <mkinoti69@gmail.com>
2017-05-21 15:44:38 +0000

    Remove category column from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521110444) do
+ActiveRecord::Schema.define(version: 20170521154215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20170521110444) do
     t.integer  "store_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "category"
     t.index ["store_id"], name: "index_products_on_store_id", using: :btree
   end
 
