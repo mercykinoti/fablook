4843bd8c7f6600d1e655df78177ecf4fe9697ab8
mercykinoti <mkinoti69@gmail.com>
2017-05-21 22:31:33 +0000

    Add relationship between Products and Categories
    This commit adds a has_and_belongs_to_many between Products and Categories and creates a join table to enforce this relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,11 @@ class Product < ApplicationRecord
 
   belongs_to :store
   has_many :reviews
+  has_and_belongs_to_many :categories
 
   validates_presence_of :name, :price, :description, :image
 
-    mount_uploader :image, AvatarUploader
+  mount_uploader :image, AvatarUploader
 
  scope :price, -> (price) { where price: price }
  scope :productname, -> (name) { where("name like ?", "#{name}%")}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521221725) do
+ActiveRecord::Schema.define(version: 20170521222309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20170521221725) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
   end
 
   create_table "choices", force: :cascade do |t|
