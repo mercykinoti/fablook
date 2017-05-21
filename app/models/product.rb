4aaa6f44951c78c1e56ae55a7e4a23223f01617c
mercykinoti <mkinoti69@gmail.com>
2017-05-21 22:22:41 +0000

    Add Category Model and Fix Carousel

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,14 @@
 class Product < ApplicationRecord
   include PgSearch
-  multisearchable :against => [:name, :description, :category, :price]
+  multisearchable :against => [:name, :description, :price]
 
   belongs_to :store
   has_many :reviews
 
-  validates_presence_of :name, :price, :description, :image, :category
+  validates_presence_of :name, :price, :description, :image
 
     mount_uploader :image, AvatarUploader
 
  scope :price, -> (price) { where price: price }
  scope :productname, -> (name) { where("name like ?", "#{name}%")}
- scope :category, -> (category) { where category: category }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521154215) do
+ActiveRecord::Schema.define(version: 20170521221725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20170521154215) do
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_admins_on_username", unique: true, using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "choices", force: :cascade do |t|
